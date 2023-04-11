78bec54698570b951f69167e5b74a7e76556ace4
Sadid Rafsun Tulon <srtulon6@gmail.com>
2023-04-11 14:43:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 from setuptools import setup
 
-setup(name='imbalanced_metrics',
-      version='0.1',
+setup(
+      name='imbalanced_metrics',
+      version='0.0.1',
       description='Perfromance metrics for imbalanced classification and imbalanced regression tasks',
+      long_description = open('README.md').read(),
+      long_description_content_type = "text/markdown",
       url='https://github.com/paobranco/ImablanceMetrics',
-      authors=['Sadid Rafsun Tulon','Jean-Gabriel Gaudreault','Paula Branco'],
-      packages=['imbalanced_metrics'],
-      install_requires=[
-          'scikit-learn','numpy','smogn',
+      author='Sadid Rafsun Tulon, Jean-Gabriel Gaudreault, Paula Branco',
+      author_email = 'stulo080@uottawa.ca, j.gaudreault@uottawa.ca, pbranco@uottawa.ca',
+      classifiers = [
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
       ],
-      zip_safe=False)
+      keywords = [
+        'imbalanced learning',
+        'classification',
+        'regression',
+        'metrics'
+      ],
+      packages=['imbalanced_metrics'],
+      
+      install_requires=[
+          'scikit-learn','numpy','smogn', 'pandas', 'numpy'
+      ],
+      include_package_data = True,
+      zip_safe=False
+      )
