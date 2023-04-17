069c4e5edda1f6626d1f0329f81e21049317d8c5
Sadid Rafsun Tulon <srtulon6@gmail.com>
2023-04-17 12:52:57 -0400

    Update tests

--- imbalance_metrics/tests/test_classification_metrics.py
+++ imbalance_metrics/tests/test_classification_metrics.py
@@ -9,8 +9,11 @@ class TestCM(unittest.TestCase):
     def setUp(self):
         self.y,self.y_pred,self.y_proba=get_y.cla()
 
+    def test_get_minority(self):
+        self.assertEqual(cm.get_minority(y_true=self.y), 0)
+
     def test_gmean_score(self):
-        self.assertEqual(cm.gmean_score(y_true=self.y, y_pred=self.y_pred), 0.41945610614608386)
+        self.assertEqual(cm.gmean_score(y_true=self.y, y_pred=self.y_pred), 0.18989464186862232)
 
     def test_pr_davis(self):
         self.assertEqual(cm.pr_davis(self.y,self.y_proba), 0.528087977617617)
